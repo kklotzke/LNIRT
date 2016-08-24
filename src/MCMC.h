7bd593bf6e8c186f3759bc5971a9c68eb49623ec
kklotzke <Konrad.Klotzke@gmail.com>
2016-08-24 12:27:11 +0200

    Rcpp headers for MCMC functions

---
+++ src/MCMC.h
@@ -0,0 +1,24 @@
+#include "RcppArmadillo.h"
+
+Rcpp::List Rcpp_DrawS_LNIRT(const arma::vec &alpha0, const arma::vec &beta0, const arma::vec &guess0, 
+                      const arma::vec &theta0, const arma::mat &Y);
+
+arma::mat Rcpp_DrawZ_LNIRT(const arma::vec &alpha0, const arma::vec &beta0, const arma::vec &theta0, 
+                           const arma::mat &S, const arma::mat &D, const arma::mat &eta, const bool PNO = false);
+
+arma::vec Rcpp_DrawTheta_LNIRT(const arma::vec &alpha0, const arma::vec &beta0, const arma::mat &Z,
+                               const arma::vec &mu, const double sigma);
+
+arma::vec Rcpp_DrawZeta_LNIRT(const arma::mat &RT, const arma::vec &phi, const arma::vec &lambda,
+                              const arma::vec &sigma2, const arma::vec &mu, const double sigmaz);
+
+arma::fvec Rcpp_DrawC_LNIRT(const arma::mat &S, const arma::mat &Y);
+
+arma::vec Rcpp_DrawBeta_LNIRT(const arma::vec &theta, const arma::vec &alpha, const arma::mat &Z,
+                              const arma::vec &mu, const double sigma);
+
+arma::vec Rcpp_DrawLambda_LNIRT(const arma::mat &RT, const arma::vec &phi, const arma::vec &zeta, const arma::vec &sigma2, 
+                                const arma::vec &mu, const double sigmal);
+
+arma::vec Rcpp_SampleB_LNIRT(const arma::mat &Y, const arma::mat &X, const arma::mat &Sigma, const arma::vec &mu, 
+                             const arma::mat &V0);
