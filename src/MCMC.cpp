6778dc0d95d70086b2b945ccacf3ced0d6825b6d
kklotzke <Konrad.Klotzke@gmail.com>
2016-09-01 14:01:37 +0200

    Rcpp implementation for SampleS and DrawLambda LNRT

--- src/MCMC.cpp
+++ src/MCMC.cpp
@@ -5,6 +5,157 @@
 using namespace Rcpp;
 
 
+// Common functions used in MCMC
+
+
+
+//'@export
+// [[Rcpp::export]]
+arma::vec Rcpp_DrawZeta(const arma::mat &RT, const arma::vec &phi, const arma::vec &lambda,
+                              const arma::vec &sigma2, const arma::vec &mu, const double sigmaz) {
+  const int N = RT.n_rows;
+  const int K = RT.n_cols;
+  
+  arma::mat l0(K, N);
+  for (int i = 0; i < N; i++) {
+    l0.col(i) = lambda;
+  }
+  
+  arma::mat Z = l0 - RT.t();
+  arma::mat sigma2inv = arma::diagmat(1 / sigma2);
+  arma::mat ee(phi.t() * sigma2inv);
+  arma::mat tmp = 1 / (ee * phi +  1 / sigmaz);
+  
+  const double vartheta = tmp(0, 0);
+  const double sqrtvartheta = sqrt(vartheta); 
+  arma::mat meantheta((ee * Z + (mu.t() / sigmaz)) * vartheta);
+  arma::vec zeta(N);
+  for (int i = 0; i < N; i++) {
+    zeta(i) = rnorm(1, meantheta(i), sqrtvartheta)(0);
+  }
+  
+  return (zeta);
+}
+
+
+
+//'@export
+// [[Rcpp::export]]
+List Rcpp_SampleB(const arma::mat &Y, const arma::vec &X, const arma::mat &Sigma, const arma::vec &B0, 
+                  const arma::mat &V0) {
+  const int k = Y.n_cols;
+  
+  arma::mat tmp = 1/(X.t() * X);
+  arma::mat Bvar = inv(inv(Sigma * tmp(0, 0)) + inv(V0));
+  arma::vec Btilde = Bvar * (inv(Sigma) * (X.t() * Y).t() + inv(V0) * B0);
+  arma::vec randN(rnorm(k, 0.0, 1.0));
+  arma::vec B = Btilde + chol(Bvar) * randN;
+  arma::mat pred = X * B.t();
+  
+  List ret; ret["B"] = B; ret["pred"] = pred;
+  return(ret);
+}
+
+
+
+//'@export
+// [[Rcpp::export]] 
+arma::mat Rcpp_rwishart(const int nu, const arma::mat V) {
+  const int m = V.n_rows;
+  arma::vec df = (nu + nu - m + 1) - arma::regspace(nu - m + 1, nu);
+  arma::mat RT;
+  if (m > 1) {
+    arma::vec tmp(m);
+    for (int i = 0; i < m; i++) {
+      tmp(i) = sqrt(rchisq(1, df(i))(0));
+    }
+    RT = arma::diagmat(tmp); 
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < m; j++) {
+        if (i > j) {
+          RT(i, j) = rnorm(1, 0.0, 1.0)(0);
+        }
+      }
+    }
+  }
+  else {
+    RT = arma::mat(1, 1);
+    RT(0,0) = sqrt(rchisq(1, df(0))(0));
+  }
+  
+  arma::mat U = chol(V);
+  arma::mat C = arma::trimatu(RT.t() * U);
+  arma::mat tmp(m, m);
+  tmp.fill(0);
+  tmp.diag() += 1;
+  arma::mat CI = solve(C, tmp);
+  arma::mat IW = CI * CI.t();
+  
+  return (IW);
+}
+
+
+
+
+// MCMC functions used in LNRT
+
+//'@export
+// [[Rcpp::export]]
+arma::vec Rcpp_SampleS_LNRT(const arma::mat &RT, const arma::vec &zeta, const arma::vec &lambda, 
+                            const arma::vec &phi, const arma::vec &ingroup) {
+  const int N = RT.n_rows;
+  const int K = RT.n_cols;
+  const int Nr = sum(ingroup);
+  const int ss0 = 10;
+  
+  arma::mat Z0(N, K);
+  arma::vec colsum(K);
+  colsum.fill(0);
+  for (int i = 0; i < N; i++) {
+    Z0.row(i) = (RT.row(i) + phi.t() * zeta(i) - lambda.t()) * ingroup(i);
+    Z0.row(i) %= Z0.row(i);
+    
+    for (int j = 0; j < K; j++) {
+      colsum(j) += Z0(i, j); 
+    }
+  }
+  
+  arma::vec randChisq(rchisq(K, Nr));
+  arma::vec sigma2 = (colsum + ss0) / randChisq;
+  
+  return (sigma2);
+}
+
+
+
+//'@export
+// [[Rcpp::export]]
+arma::vec Rcpp_DrawLambda_LNRT(const arma::mat &RT, const arma::vec &zeta, const arma::vec &sigma2, 
+                               const arma::vec &mu, const double sigma) {
+  const int N = RT.n_rows;
+  const int K = RT.n_cols;
+  
+  arma::mat RT0(N, K);
+  for (int i = 0; i < K; i++) {
+    RT0.col(i) = RT.col(i) + zeta;
+  }
+  
+  arma::mat XX(N, K);
+  XX.fill(1);
+  arma::mat tmp = (XX.t() * XX);
+  arma::vec pvar = tmp.diag() / sigma2 + 1 / sigma;
+  arma::vec sqrtpvar = sqrt(1 / pvar);
+  tmp = (XX.t() * RT);
+  arma::vec betahat = tmp.diag() / sigma2;
+  arma::vec mu0 = (betahat + mu / sigma) / pvar;
+  arma::vec beta(K);
+  for (int i = 0; i < K; i++) {
+    beta(i) = rnorm(1, mu0(i), sqrtpvar(i))(0);
+  }
+  
+  return (beta);
+}
+
 
 
 // MCMC functions used in LNIRT 
@@ -39,6 +190,7 @@ List Rcpp_DrawS_LNIRT(const arma::vec &alpha0, const arma::vec &beta0, const arm
   List ret; ret["S"] = S % Y; ret["eta"] = etamat;
   return(ret);
 }
+
 
 
 //'@export
@@ -103,36 +255,6 @@ arma::vec Rcpp_DrawTheta_LNIRT(const arma::vec &alpha0, const arma::vec &beta0, 
   }
   
   return (theta);
-}
-
-
-
-//'@export
-// [[Rcpp::export]]
-arma::vec Rcpp_DrawZeta_LNIRT(const arma::mat &RT, const arma::vec &phi, const arma::vec &lambda,
-                              const arma::vec &sigma2, const arma::vec &mu, const double sigmaz) {
-  const int N = RT.n_rows;
-  const int K = RT.n_cols;
-  
-  arma::mat l0(K, N);
-  for (int i = 0; i < N; i++) {
-    l0.col(i) = lambda;
-  }
-  
-  arma::mat Z = l0 - RT.t();
-  arma::mat sigma2inv = arma::diagmat(1 / sigma2);
-  arma::mat ee(phi.t() * sigma2inv);
-  arma::mat tmp = 1 / (ee * phi +  1 / sigmaz);
-  
-  const double vartheta = tmp(0, 0);
-  const double sqrtvartheta = sqrt(vartheta); 
-  arma::mat meantheta((ee * Z + (mu.t() / sigmaz)) * vartheta);
-  arma::vec zeta(N);
-  for (int i = 0; i < N; i++) {
-    zeta(i) = rnorm(1, meantheta(i), sqrtvartheta)(0);
-  }
-  
-  return (zeta);
 }
 
 
@@ -218,7 +340,7 @@ arma::vec Rcpp_DrawLambda_LNIRT(const arma::mat &RT, const arma::vec &phi, const
   sigmaldiag.fill(0);
   sigmaldiag.diag() += sigmal;
   
-  List sampleB = Rcpp_SampleB_LNIRT(Z0, X, sigma2diag, mu, sigmaldiag);
+  List sampleB = Rcpp_SampleB(Z0, X, sigma2diag, mu, sigmaldiag);
   arma::vec lambda = as<arma::vec>(sampleB["B"]);
 
   return (lambda);
@@ -272,12 +394,11 @@ arma::vec Rcpp_DrawPhi_LNIRT(const arma::mat &RT, const arma::vec &lambda, const
   sigmaldiag.fill(0);
   sigmaldiag.diag() += sigmal;
   
-  List sampleB = Rcpp_SampleB_LNIRT(Z0, zeta, sigma2diag, mu, sigmaldiag);
+  List sampleB = Rcpp_SampleB(Z0, zeta, sigma2diag, mu, sigmaldiag);
   arma::vec phi = as<arma::vec>(sampleB["B"]);
   
   return (phi);
 }
-
 
 
 
@@ -309,59 +430,6 @@ arma::vec Rcpp_SampleS2_LNIRT(const arma::mat &RT, const arma::vec &zeta, const 
 
 
 
-//'@export
-// [[Rcpp::export]]
-List Rcpp_SampleB_LNIRT(const arma::mat &Y, const arma::vec &X, const arma::mat &Sigma, const arma::vec &B0, 
-                                const arma::mat &V0) {
-  const int k = Y.n_cols;
 
-  arma::mat tmp = 1/(X.t() * X);
-  arma::mat Bvar = inv(inv(Sigma * tmp(0, 0)) + inv(V0));
-  arma::vec Btilde = Bvar * (inv(Sigma) * (X.t() * Y).t() + inv(V0) * B0);
-  arma::vec randN(rnorm(k, 0.0, 1.0));
-  arma::vec B = Btilde + chol(Bvar) * randN;
-  arma::mat pred = X * B.t();
-  
-  List ret; ret["B"] = B; ret["pred"] = pred;
-  return(ret);
-}
- 
 
- 
-//'@export
-// [[Rcpp::export]] 
-arma::mat Rcpp_rwishart_LNIRT(const int nu, const arma::mat V) {
-  const int m = V.n_rows;
-  arma::vec df = (nu + nu - m + 1) - arma::regspace(nu - m + 1, nu);
-  arma::mat RT;
-  if (m > 1) {
-    arma::vec tmp(m);
-    for (int i = 0; i < m; i++) {
-      tmp(i) = sqrt(rchisq(1, df(i))(0));
-    }
-    RT = arma::diagmat(tmp); 
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < m; j++) {
-        if (i > j) {
-          RT(i, j) = rnorm(1, 0.0, 1.0)(0);
-        }
-      }
-    }
-  }
-  else {
-    RT = arma::mat(1, 1);
-    RT(0,0) = sqrt(rchisq(1, df(0))(0));
-  }
-  
-  arma::mat U = chol(V);
-  arma::mat C = arma::trimatu(RT.t() * U);
-  arma::mat tmp(m, m);
-  tmp.fill(0);
-  tmp.diag() += 1;
-  arma::mat CI = solve(C, tmp);
-  arma::mat IW = CI * CI.t();
-  
-  return (IW);
 
-}
- 
