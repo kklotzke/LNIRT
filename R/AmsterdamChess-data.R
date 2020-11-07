1915841219df4b8343de851188a8dadbbe263b1c
Konrad Klotzke <Konrad.Klotzke@gmail.com>
2020-11-07 16:27:39 +0100

    Add AmsterdamChess and CredentialForm1 data

---
+++ R/AmsterdamChess-data.R
@@ -0,0 +1,16 @@
+#' Amsterdam Chess Test (ACT) data
+#'
+#' Responses and response time data from the Amsterdam Chess Test (ACT).
+#'
+#' @docType data
+#'
+#' @usage data(AmsterdamChess)
+#'
+#' @format A dataframe with 259 rows and 81 variables.
+#'
+#' @keywords datasets
+#'
+#' @references van der Maas, H. L., & Wagenmakers, E. J. (2005). A psychometric analysis of chess expertise. The American journal of psychology, 118(1), 29-60.
+#' (\href{https://pubmed.ncbi.nlm.nih.gov/15822609}{PubMed})
+#'
+"AmsterdamChess"
---
+++ R/CredentialForm1-data.R
@@ -0,0 +1,16 @@
+#' Credential Form data
+#'
+#' Responses and response time data from the credential data set of Cizek and Wollack (2016).
+#'
+#' @docType data
+#'
+#' @usage data(CredentialForm1)
+#'
+#' @format A dataframe with 1636 rows and 610 variables.
+#'
+#' @keywords datasets
+#'
+#' @references Cizek GJ, Wollack JA (eds.) (2016). Handbook of Quantitative Methods for Detecting Cheating on Tests. Routledge.
+#' (\href{https://www.taylorfrancis.com/books/9781315743097}{Taylor&Francis})
+#'
+"CredentialForm1"
