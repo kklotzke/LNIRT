05f982d7d74bd791ef2ef6d4d5e6791d7413243a
Konrad Klotzke <Konrad.Klotzke@gmail.com>
2020-11-10 18:09:31 +0100

    Add missing character to startup message

--- R/init.R
+++ R/init.R
@@ -9,7 +9,7 @@
 .onAttach <- function(libname, pkgname) {
   start_message <- c(" \n"
                      , "   LNIRT v", packageDescription("LNIRT")$Version, " (log-normal RT-IRT Modeling)",  "\n"
-                     , "   ", rep('-', 40), "\n"
+                     , "   ", rep('-', 41), "\n"
                      , "   J.-P. Fox & K. Klotzke \n", sep = "")
                      #, "   ", rep('-', 20), "\n", sep = "")
   
